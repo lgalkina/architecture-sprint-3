1db26e7323e1b24f15ec66d908b462ccb95e649d
Lyudmila Galkina <lygalkina@ozon.ru>
2024-10-26 17:48:00 +0300

    fix

--- terraform/smart-home-monolith.tf
+++
@@ -1,6 +0,0 @@
-resource "helm_release" "smart-home-monolith" {
-  name       = "smart-home-monolith"
-  namespace  = "default"
-  chart      = "../charts/smart-home-monolith"
-  timeout    = 1800  # Increase the timeout to 30 minutes
-}
